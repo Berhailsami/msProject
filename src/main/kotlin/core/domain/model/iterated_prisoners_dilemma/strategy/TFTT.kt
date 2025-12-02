59aba9fbfb112a913c487f3740caf5e10a0332d7
berhailsami <berhailsami@outlook.fr>
2025-12-02 12:55:53 +0100

    project 4 v2

--- src/main/kotlin/core/domain/model/iterated_prisoners_dilemma/strategy/TFTT.kt
+++ src/main/kotlin/core/domain/model/iterated_prisoners_dilemma/strategy/TFTT.kt
@@ -4,25 +4,27 @@ import core.domain.model.iterated_prisoners_dilemma.Move
 
 class TFTT : IPDStrategy {
     override val name = "TFTT"
-    private var opponentLastMove: Move? = null
-    private var opponentSecondLastMove: Move? = null
+
+    private var historyLastMove: Move? = null
+    private var historySecondLastMove: Move? = null
 
     override fun play(opponentLastMove: Move?): Move {
-        val decision = if (this.opponentLastMove == Move.DEFECT && opponentLastMove == Move.DEFECT) {
+        if (opponentLastMove == null) {
+            return Move.COOPERATE
+        }
+
+        this.historySecondLastMove = this.historyLastMove
+        this.historyLastMove = opponentLastMove
+
+        return if (historyLastMove == Move.DEFECT && historySecondLastMove == Move.DEFECT) {
             Move.DEFECT
         } else {
             Move.COOPERATE
         }
-
-        // Update history for the next round
-        this.opponentSecondLastMove = this.opponentLastMove
-        this.opponentLastMove = opponentLastMove
-
-        return decision
     }
 
     fun reset() {
-        opponentLastMove = null
-        opponentSecondLastMove = null
+        historyLastMove = null
+        historySecondLastMove = null
     }
 }
